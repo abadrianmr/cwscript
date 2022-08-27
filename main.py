bbf55e0043d0c27e183e13ad2da541ac48316d89
abadrianmr <43829910+abadrianmr@users.noreply.github.com>
2022-08-27 08:17:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ with open('messages.json') as f:
 
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
-cws_id = os.environ.get('CONF_CHAT_ID')
+cws_id = int(os.environ.get('CONF_CHAT_ID'))
 session = os.environ.get('SESSION')
 client = TelegramClient(StringSession(session), api_id, api_hash).start()
 print("Hello")
