f79ea7cfd0a55a1c4666df4c52de8afa9bae2342
4milrod <4Milrod@gmail.com>
2022-11-20 19:17:21 +0100

    Schedule

--- main.py
+++ main.py
@@ -1,99 +1,89 @@
-from cgitb import text
+import asyncio
 import json
 import re
 import os
+from pytz import utc
 from random import randint
-import sys
-import datetime as dt
-from datetime import datetime
-import aiocron
-from telethon import TelegramClient, events, sync
-from telethon.events.newmessage import NewMessage
+from telethon import TelegramClient, events, sync, functions, types
+from telethon.tl.functions.account import UpdateProfileRequest
 from telethon.sessions import StringSession
-import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.combining import OrTrigger
+from apscheduler.triggers.cron import CronTrigger
+from telethon.events.newmessage import NewMessage
+
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')        
+session = os.environ.get('SESSION')
+cws_id = int(os.environ.get('CONF_CHAT_ID'))
+
+clients = []
+scheduler: AsyncIOScheduler = AsyncIOScheduler(timezone=utc)
+with open('messages.json') as f:
+    regex_msg = json.load(f)
 
 class TClient:
     client: TelegramClient
-    regex_msg: None
-    repository: None
+    cws_id: int
 
-    async def main(self):
-        """Start the bot."""
-        # Create the Updater and pass it your bot's token.
-        # Make sure to set use_context=True to use the new context based callbacks
-        # Post version 12 this will no longer be necessary
-        print("Starting running...")
-        sys.stdout.flush()
+    async def ping(self, event: events.NewMessage):
+        await self.client.send_message(self.cws_id, "pong")
 
-        with open('messages.json') as f:
-            self.regex_msg = json.load(f)
-
-        api_id = os.environ.get('API_ID')
-        api_hash = os.environ.get('API_HASH')        
-        session = os.environ.get('SESSION')
-        self.cws_id = int(os.environ.get('CONF_CHAT_ID'))
+    def __init__( self, session, api_id, api_hash, cws_id ):
+        self.client = TelegramClient(StringSession(session), api_id, api_hash)
+        self.cws_id = cws_id
         self.mobs_id = -1001408823679
-
-        self.client: TelegramClient = await TelegramClient(StringSession(session), api_id, api_hash).start()       
+        self.client.add_event_handler(self.ping, events.NewMessage(from_users='me', chats=cws_id, pattern="ping"))
         self.client.add_event_handler(self.cw_msg_handler, events.NewMessage(incoming=True, from_users='chtwrsbot'))
-        self.client.add_event_handler(self.orders_msg_handler, events.NewMessage(incoming=True, from_users='botniatobot'))
-        self.client.add_event_handler(self.foray_handler, events.NewMessage(incoming=True, from_users='chtwrsbot', pattern=self.regex_msg["foray"]))
-        self.client.add_event_handler(self.trader_handler, events.NewMessage(from_users='chtwrsbot', pattern=self.regex_msg["trader"]))
-        self.client.add_event_handler(self.config_trader_handler, events.NewMessage(from_users='me', chats=self.cws_id,pattern=self.regex_msg["traderConfig"]))
-        self.client.add_event_handler(self.mobs_handler, events.NewMessage(from_users='chtwrsbot', pattern=self.regex_msg["mobs"]))
-        print("Running...")
-        sys.stdout.flush()        
+        self.client.add_event_handler(self.foray_handler, events.NewMessage(incoming=True, from_users='chtwrsbot', pattern=regex_msg["foray"]))
+        self.client.add_event_handler(self.trader_handler, events.NewMessage(from_users='chtwrsbot', pattern=regex_msg["trader"]))
+        self.client.add_event_handler(self.mobs_handler, events.NewMessage(from_users='chtwrsbot', pattern=regex_msg["mobs"]))
 
-        await self.client.send_message(self.cws_id, "Running...")        
-        await self.client.run_until_disconnected()        
-
-    @aiocron.crontab('*/1 * * * *')
-    async def show_time(self):
-        await self.client.send_message(self.cws_id, "Hello")
-
-    async def send_order(self):
-        await self.client.send_message(self.cws_id, "🛡Defend")
+    async def start(self):
+        await self.client.start()
     
-    async def cw_msg_handler(self, event: NewMessage.Event):        
-        await self.client.forward_messages(self.cws_id, event.message)  
-        
-    async def orders_msg_handler(self, event: NewMessage.Event):  
-        pattern1 = re.compile(self.regex_msg["order1"])
-        pattern2 = re.compile(self.regex_msg["order2"])
-        pattern3 = re.compile(self.regex_msg["order3"])
-        if(pattern1.match(event.raw_text)):     
-            await self.client.send_message('botniatobot', "/order")
-        elif pattern2.match(event.raw_text):
-            index = event.raw_text.find('/')
-            delay = randint(2,5)  
-            await asyncio.sleep(delay) 
-            await self.client.send_message('botniatobot', event.raw_text[index:])
-        elif pattern3.match(event.raw_text):
-            order = re.search(r'.*(\(https:\/\/t.me\/share\/url\?url=(.*?)\))', event.raw_text).group(2)
-            await self.client.send_message(self.cws_id, order)
-            #await self.client.send_message(self.cws_id, schedule=)
+    async def run(self):
+        await self.client.send_message(self.cws_id, "Running...")
+        await self.client.run_until_disconnected()
 
-    async def foray_handler(self, event: NewMessage.Event): 
-        delay = randint(30, 60)  
-        await self.client.send_message(self.cws_id, f"Sending in: {delay} seconds.") 
-        await asyncio.sleep(delay)    
-        await event.message.click(0)
+    async def sendOrder(self):
+        await self.client.send_message('chtwrsbot', "🛡Defend")
     
-    async def trader_handler(self, event: NewMessage.Event): 
-        delay = randint(10,20)  
-        await self.client.send_message(self.cws_id, f"Sending in: {delay} seconds.") 
-        await asyncio.sleep(delay)    
-        ammount = re.findall(r'\b\d+\b', event.raw_text)
-        await self.client.send_message('chtwrsbot', f"/sc 02 {ammount[1]}", )     
-
     async def mobs_handler(self, event: NewMessage.Event):
         delay = randint(5, 20)  
         await self.client.send_message(self.cws_id, f"Sending mob to group: {delay} seconds.") 
         await asyncio.sleep(delay)
         await self.client.forward_messages(self.mobs_id, event.message)
 
-    async def config_trader_handler(self, event: NewMessage.Event):
-        sender = await event.get_sender()
+    async def cw_msg_handler(self, event: NewMessage.Event):        
+        await self.client.forward_messages(self.cws_id, event.message)
 
-if __name__ == '__main__':
-    asyncio.run(TClient().main())
+    async def foray_handler(self, event: NewMessage.Event): 
+        delay = randint(30, 60)  
+        await self.client.send_message(self.cws_id, f"Sending in: {delay} seconds.") 
+        await asyncio.sleep(delay)    
+        await event.message.click(0)
+
+    async def trader_handler(self, event: NewMessage.Event): 
+        delay = randint(10,20)  
+        await self.client.send_message(self.cws_id, f"Sending in: {delay} seconds.") 
+        await asyncio.sleep(delay)    
+        ammount = re.findall(r'\b\d+\b', event.raw_text)
+        await self.client.send_message('chtwrsbot', f"/sc 02 {ammount[1]}", )
+
+async def setOrder():
+    t: TClient
+    for t in clients:
+        await t.sendOrder()                
+
+async def main():    
+    scheduler.add_job(setOrder, trigger="cron", hour='6,14,22', minute=18)
+    scheduler.start()
+    clients.append(TClient(session, api_id, api_hash, cws_id))
+    t: TClient
+    for t in clients:
+        await t.start()        
+        await t.run()
+
+if __name__ == '__main__':  
+    asyncio.run(main())
